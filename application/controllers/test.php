9bb8bb3204e9244c5783cc2b2df0b6c6498e465e
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2012-11-14 17:06:12 -0500

    Initial setup of laravel

---
+++ application/controllers/test.php
@@ -0,0 +1,15 @@
+<?php
+
+class Test_Controller extends Base_Controller {
+
+	public function action_index()
+	{
+		//return View::make('home.index');
+	}
+	
+	public function action_new()
+	{
+		echo 'Test';
+	}
+
+}
